0ecf5f5256d06a12fd64fe63c9ca4c697fc526c0
unknown <recruiterhrpartner4@gmail.com>
2025-10-20 13:26:09 +0300

    style: changed class names

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var createNewTaskElement = function(taskString) {
   editInput.className = "task";
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
   editButton.className = "edit";
-  deleteButton.className = "delete";
+  deleteButton.className = "delete-item";
   deleteButtonImg.src = "./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
   //and appending.
@@ -111,7 +111,7 @@ var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
   //select ListItems children
   var checkBox = taskListItem.querySelector("input[type=checkbox]");
   var editButton = taskListItem.querySelector("button.edit");
-  var deleteButton = taskListItem.querySelector(".delete");
+  var deleteButton = taskListItem.querySelector(".delete-item");
   //Bind editTask to edit button.
   editButton.onclick = editTask;
   //Bind deleteTask to delete button.
