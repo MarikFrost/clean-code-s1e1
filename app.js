eee726b8f085d5388291b8fe683a28c8f601b042
unknown <recruiterhrpartner4@gmail.com>
2025-10-20 12:50:20 +0300

    style: edit tag selectors

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
   //select ListItems children
   var checkBox = taskListItem.querySelector("input[type=checkbox]");
   var editButton = taskListItem.querySelector("button.edit");
-  var deleteButton = taskListItem.querySelector("button.delete");
+  var deleteButton = taskListItem.querySelector(".delete");
   //Bind editTask to edit button.
   editButton.onclick = editTask;
   //Bind deleteTask to delete button.
